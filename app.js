1ab8fd06bee19364380ecfed7b210b0b9b5c82d2
kuong <kseng2906@gmail.com>
2019-08-04 20:01:35 +1000

    add passport

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var bodyParser  = require("body-parser");
 var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
 var methodOverride = require('method-override');
 var Fashion = require("./models/fashion");
 var Comment = require("./models/comment");
@@ -15,6 +17,26 @@ app.use(express.static(__dirname + "/public")); // css, setting the directory to
 app.use(bodyParser.urlencoded({extended: true})); // this is needed to read the body parser
 app.set("view engine", "ejs");
 app.use(methodOverride('_method'));
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// making currentUser availiable in all routes -> making it global? 
+app.use(function(req, res, next){
+   	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+   	next();
+});
 
 // redirect to homepage
 app.get("/", (req, res) => {
--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 var commentSchema = mongoose.Schema({
 	text: String,
-	author: String
+	username: String
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
